f946f16aac9ab091465385b0916083b019105088
Adrian Labbe <adrilabbelol@gmail.com>
2017-12-29 13:03:35 -0300

    Included missing file

--- Pisth/Helpers/Data+html2AttributedString.swift
+++ Pisth/Helpers/Data+html2AttributedString.swift
@@ -3,7 +3,20 @@
 //  Pisth
 //
 //  Created by Adrian on 28.12.17.
-//  Copyright © 2017 ADA. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension Data {
+    var html2AttributedString: NSAttributedString? {
+        do {
+            return try NSAttributedString(data: self, options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding: String.Encoding.utf8.rawValue], documentAttributes: nil)
+        } catch {
+            print("error:", error)
+            return  nil
+        }
+    }
+    var html2String: String {
+        return html2AttributedString?.string ?? ""
+    }
+}
