2370faa57d5c35e8afea8731f21cec9d8372127b
Adrian Labbe <adrilabbelol@gmail.com>
2017-12-31 10:38:42 -0300

    Removed alias for text editors and remove startup commands from history

--- Pisth/Helpers/ShellStartup.swift
+++ Pisth/Helpers/ShellStartup.swift
@@ -12,26 +12,23 @@ class ShellStartup {
     // Commands to run when open SSH shell
     static let commands = [
                            // Print ClEaRtHeScReEnNoW instead of clear shell
-                           "alias clear='echo Cl\\EaRtHeScReEnNoW'",
+                           "alias clear='echo Cl\\EaRtHeScReEnNoW'; history -d $(history 1)",
                            
                            
                            // Sync history
                            
                            // Avoid duplicates
-                           "export HISTCONTROL=ignoredups:erasedups",
+                           "export HISTCONTROL=ignoredups:erasedups; history -d $(history 1)",
                            
                            // When the shell exits, append to the history file instead of overwriting it
                            "shopt -s histappend",
                            
                            // After each command, append to the history file and reread it
-                           "export PROMPT_COMMAND=\"${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}history -a; history -c; history -r\"",
+                           "export PROMPT_COMMAND=\"${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}history -a; history -c; history -r\"; history -d $(history 1)",
                            
                            // Create .pisth_history symlink of history file
-                           "ln -s $HISTFILE .pisth_history",
+                           "ln -s $HISTFILE .pisth_history > /dev/null; history -d $(history 1)",
                            
-                           // Disable nano, vim and emacs
-                           "alias nano='nano is not supported by Pisth, edit files from file browser'",
-                           "alias vim='vim is not supported by Pisth, edit files from file browser'",
-                           "alias emacs='emacs is not supported by Pisth, edit files from file browser'"
+                           ""
     ]
 }
