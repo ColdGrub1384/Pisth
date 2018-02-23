117390a34f6ee222d594caa1095d5ee6e55fec91
Adrian Labbe <adrilabbelol@gmail.com>
2018-02-23 18:27:26 -0300

    Edit connections with a Table view controller instead of an alert

--- Pisth/ViewControllers/Classes/BookmarksTableViewController.swift
+++ Pisth/ViewControllers/Classes/BookmarksTableViewController.swift
@@ -49,220 +49,16 @@ class BookmarksTableViewController: UITableViewController, GADBannerViewDelegate
     ///     - index: Index of connection to edit.
     func showInfoAlert(editInfoAt index: Int? = nil) {
         
-        // Title of alert
-        var title: String {
-            if index == nil {
-                return "Add new connection"
-            } else {
-                return "Edit connection"
-            }
-
+        guard let editConnectionVC = UIStoryboard(name: "ConnectionInfoTableViewController", bundle: Bundle.main).instantiateInitialViewController() as? ConnectionInformationTableViewController else {
+            return
         }
         
-        // Message of alert
-        var message: String {
-            if index == nil {
-                return "Fill fields to add a connection to bookmarks"
-            } else {
-                return "Fill fields to edit bookmark"
-            }
-            
-        }
-        
-        // Alert to be displayed
-        let addNewAlertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        
-        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Connection")
-        request.returnsObjectsAsFaults = false
-        
-        // Name
-        addNewAlertController.addTextField { (name) in // Optional
-            name.placeholder = "Nickname (Optional)"
-        }
-        
-        // Host
-        addNewAlertController.addTextField { (host) in // Requierd
-            host.placeholder = "Hostname / IP address"
-        }
-        
-        // Port
-        addNewAlertController.addTextField { (port) in // Optional
-            port.placeholder = "Port (22)"
-            port.keyboardType = .numberPad
-        }
-        
-        // Username
-        addNewAlertController.addTextField { (username) in // Requierd
-            username.placeholder = "Username"
-        }
-        
-        // Password
-        addNewAlertController.addTextField { (password) in // Optional
-            password.placeholder = "Password"
-            password.isSecureTextEntry = true
-        }
-        
-        // SFTP
-        addNewAlertController.addTextField { (sftp) in // Requierd
-            sftp.text = "Don't use SFTP"
-            
-            let inputView = BooleanInputView(title: "Use SFTP", textField: sftp, on: "Use SFTP", off: "Don't use SFTP", currentState: false, frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 200))
-            inputView.completion = { (isOn) in
-                
-                let pathTextField = addNewAlertController.textFields![6]
-                    
-                pathTextField.isEnabled = isOn
-            }
-            
-            sftp.inputView = inputView
-        }
-        
-        // Path
-        addNewAlertController.addTextField { (path) in // Optional
-            path.placeholder = "Path (~)"
-            path.isEnabled = false
-        }
-        
-        // Fill info with given index
         if let index = index {
-            let connection = DataManager.shared.connections[index]
-            
-            addNewAlertController.textFields![0].text = connection.name
-            addNewAlertController.textFields![1].text = connection.host
-            addNewAlertController.textFields![2].text = "\(connection.port)"
-            addNewAlertController.textFields![3].text = connection.username
-            addNewAlertController.textFields![4].text = connection.password
-            addNewAlertController.textFields![6].text = connection.path
-            
-            if connection.useSFTP {
-                addNewAlertController.textFields![5].text = "Use SFTP"
-                (addNewAlertController.textFields![5].inputView as! BooleanInputView).currentState = true
-                
-                addNewAlertController.textFields![6].isEnabled = true
-            }
+            editConnectionVC.connection = DataManager.shared.connections[index]
+            editConnectionVC.index = index
         }
         
-        // Create connection
-        let addAction = UIAlertAction(title: "Save", style: .default) { (action) in
-            let name = addNewAlertController.textFields![0].text!
-            let host = addNewAlertController.textFields![1].text!
-            var port = addNewAlertController.textFields![2].text!
-            let username = addNewAlertController.textFields![3].text!
-            let password = addNewAlertController.textFields![4].text!
-            var path = addNewAlertController.textFields![6].text!
-            let useSFTP = (addNewAlertController.textFields![5].text! == "Use SFTP")
-            
-            // Check for requierd fields
-            if host == "" || username == "" {
-                self.present(addNewAlertController, animated: true, completion: {
-                    if host == "" {
-                        addNewAlertController.textFields![1].backgroundColor = .red
-                    } else {
-                        addNewAlertController.textFields![1].backgroundColor = .white
-                    }
-                    
-                    if username == "" {
-                        addNewAlertController.textFields![3].backgroundColor = .red
-                    } else {
-                        addNewAlertController.textFields![3].backgroundColor = .white
-                    }
-                })
-            } else {
-                if port == "" { // Port is 22 by default
-                    port = "22"
-                }
-                
-                if path == "" { // Path is ~ by default
-                    path = "~"
-                }
-                
-                if let port = UInt64(port) {
-                    DataManager.shared.addNew(connection: RemoteConnection(host: host, username: username, password: password, name: name, path: path, port: port, useSFTP: useSFTP))
-                    
-                    self.tableView.reloadData()
-                } else {
-                    self.present(addNewAlertController, animated: true, completion: {
-                        addNewAlertController.textFields![2].backgroundColor = .red
-                    })
-                }
-            }
-            
-        }
-        
-        // Edit selected connection
-        let editAction = UIAlertAction(title: "Save", style: .default) { (action) in
-            let name = addNewAlertController.textFields![0].text!
-            let host = addNewAlertController.textFields![1].text!
-            var port = addNewAlertController.textFields![2].text!
-            let username = addNewAlertController.textFields![3].text!
-            let password = addNewAlertController.textFields![4].text!
-            var path = addNewAlertController.textFields![6].text!
-            let useSFTP = (addNewAlertController.textFields![5].text == "Use SFTP")
-            
-            // Check for requierd fields
-            if host == "" || username == "" {
-                self.present(addNewAlertController, animated: true, completion: {
-                    if host == "" {
-                        addNewAlertController.textFields![1].backgroundColor = .red
-                    } else {
-                        addNewAlertController.textFields![1].backgroundColor = .white
-                    }
-                    
-                    if username == "" {
-                        addNewAlertController.textFields![3].backgroundColor = .red
-                    } else {
-                        addNewAlertController.textFields![3].backgroundColor = .white
-                    }
-                })
-            } else {
-                if port == "" { // Port is 22 by default
-                    port = "22"
-                }
-                
-                if path == "" { // Path is ~ by default
-                    path = "~"
-                }
-                
-                if let port = UInt64(port) {
-                    let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Connection")
-                    request.returnsObjectsAsFaults = false
-                    
-                    do {
-                        let result = try (AppDelegate.shared.coreDataContext.fetch(request) as! [NSManagedObject])[index!]
-                        let passKey = String.random(length: 100)
-                        result.setValue(name, forKey: "name")
-                        result.setValue(host, forKey: "host")
-                        result.setValue(port, forKey: "port")
-                        result.setValue(username, forKey: "username")
-                        result.setValue(passKey, forKey: "password")
-                        result.setValue(path, forKey: "path")
-                        result.setValue(useSFTP, forKey: "sftp")
-                        KeychainWrapper.standard.set(password, forKey: passKey)
-                        
-                        AppDelegate.shared.saveContext()
-                    } catch let error {
-                        print("Error retrieving connections: \(error.localizedDescription)")
-                    }
-                    
-                    self.tableView.reloadData()
-                } else {
-                    self.present(addNewAlertController, animated: true, completion: {
-                        addNewAlertController.textFields![2].backgroundColor = .red
-                    })
-                }
-            }
-            
-        }
-        
-        if index != nil { // Edit connection
-            addNewAlertController.addAction(editAction)
-        } else { // Add connection
-            addNewAlertController.addAction(addAction)
-        }
-        
-        addNewAlertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-        
-        present(addNewAlertController, animated: true, completion: nil)
+        navigationController?.pushViewController(editConnectionVC, animated: true)
     }
     
     
@@ -323,6 +119,8 @@ class BookmarksTableViewController: UITableViewController, GADBannerViewDelegate
                 session.disconnect()
             }
         }
+        
+        tableView.reloadData()
         
         ConnectionManager.shared.session = nil
         ConnectionManager.shared.filesSession = nil
---
+++ Pisth/ViewControllers/Classes/ConnectionInfoTableViewController.swift
@@ -0,0 +1,150 @@
+// This source file is part of the https://github.com/ColdGrub1384/Pisth open source project
+//
+// Copyright (c) 2017 - 2018 Adrian Labbé
+// Licensed under Apache License v2.0
+//
+// See https://raw.githubusercontent.com/ColdGrub1384/Pisth/master/LICENSE for license information
+
+import UIKit
+import SwiftKeychainWrapper
+import CoreData
+
+/// Table view controller containing information about a connection.
+class ConnectionInformationTableViewController: UITableViewController {
+    
+    /// Existing connection to edit.
+    var connection: RemoteConnection?
+    
+    /// Index of connection to edit.
+    var index: Int?
+    
+    // MARK: - View controller
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        textFields = [name, host, port, username, password, path]
+        
+        for field in textFields {
+            field.autocorrectionType = .no
+            
+            let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 44))
+            toolbar.barStyle = .black
+            toolbar.items = [UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil), UIBarButtonItem(title: "Done", style: .done, target: field, action: #selector(field.resignFirstResponder))]
+            field.inputAccessoryView = toolbar
+        }
+        
+        if let connection = connection {
+            name.text = connection.name
+            host.text = connection.host
+            port.text = "\(connection.port)"
+            username.text = connection.username
+            password.text = connection.password
+            useSFTP.isOn = connection.useSFTP
+            path.text = connection.path
+        }
+        
+        if #available(iOS 11.0, *) {
+            navigationItem.largeTitleDisplayMode = .never
+        }
+    }
+    
+    /// Save connection.
+    ///
+    /// - Parameters:
+    ///     - sender: Sender object.
+    @IBAction func save(_ sender: Any) {
+        let name = self.name.text!
+        let host = self.host.text!
+        var port = self.port.text!
+        let username = self.username.text!
+        let password = self.password.text!
+        var path = self.path.text!
+        let useSFTP = self.useSFTP.isOn
+        
+        // Check for requierd fields
+        if host == "" || username == "" {
+            if host == "" {
+                self.host.backgroundColor = .red
+            } else {
+                self.host.backgroundColor = .clear
+            }
+            
+            if username == "" {
+                self.username.backgroundColor = .red
+            } else {
+                self.username.backgroundColor = .clear
+            }
+        } else {
+            if port == "" { // Port is 22 by default
+                port = "22"
+            }
+            
+            if path == "" { // Path is ~ by default
+                path = "~"
+            }
+            
+            if let port = UInt64(port) {
+                
+                if let index = index {
+                    let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Connection")
+                    request.returnsObjectsAsFaults = false
+                    
+                    do {
+                        let result = try (AppDelegate.shared.coreDataContext.fetch(request) as! [NSManagedObject])[index]
+                        let passKey = String.random(length: 100)
+                        result.setValue(name, forKey: "name")
+                        result.setValue(host, forKey: "host")
+                        result.setValue(port, forKey: "port")
+                        result.setValue(username, forKey: "username")
+                        result.setValue(passKey, forKey: "password")
+                        result.setValue(path, forKey: "path")
+                        result.setValue(useSFTP, forKey: "sftp")
+                        KeychainWrapper.standard.set(password, forKey: passKey)
+                        
+                        AppDelegate.shared.saveContext()
+                    } catch let error {
+                        print("Error retrieving connections: \(error.localizedDescription)")
+                    }
+                    
+                    navigationController?.popViewController(animated: true)
+                } else {
+                    DataManager.shared.addNew(connection: RemoteConnection(host: host, username: username, password: password, name: name, path: path, port: port, useSFTP: useSFTP))
+                    
+                    navigationController?.popViewController(animated: true)
+                }
+                
+            } else {
+                self.port.backgroundColor = .red
+            }
+        }
+    }
+    
+    // MARK: - Fields
+    
+    /// Name field.
+    @IBOutlet weak var name: UITextField!
+    
+    /// Host field.
+    @IBOutlet weak var host: UITextField!
+    
+    /// Port field.
+    @IBOutlet weak var port: UITextField!
+    
+    /// Username field.
+    @IBOutlet weak var username: UITextField!
+    
+    /// Password field.
+    @IBOutlet weak var password: UITextField!
+    
+    /// Use SFTP field.
+    @IBOutlet weak var useSFTP: UISwitch!
+    
+    /// Path field.
+    @IBOutlet weak var path: UITextField!
+    
+    /// All text fields
+    var textFields: [UITextField]!
+    
+    
+}
