a63bca24b6dc7ffc932d9105599d244e5fca6fe4
Adrian Labbe <adrilabbelol@gmail.com>
2018-03-24 12:15:23 -0300

    Updated Pisth API docs build script

--- Pisth API/build-documentation.sh
+++ Pisth API/build-documentation.sh
@@ -1,2 +1,2 @@
-jazzy --module Pisth_API -d https://pisth.github.io/docs/api/docsets/Pisth.xml -g https://github.com/ColdGrub1384/Pisth --documentation=Guides/* Getting\ Started --output ../docs/api
+jazzy --module Pisth_API -d https://pisth.github.io/docs/api/docsets/Pisth.xml -g https://github.com/ColdGrub1384/Pisth --documentation=Guides/* Getting\ Started --output ../docs/
 rm -r build
