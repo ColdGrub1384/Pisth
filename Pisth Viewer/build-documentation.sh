e22c54636db741e49335244989b09776c31779bd
Adrian Labbe <adrilabbelol@gmail.com>
2018-03-24 11:46:32 -0300

    Only build documentation for the API

--- Pisth Viewer/build-documentation.sh
+++
@@ -1,2 +0,0 @@
-jazzy --min-acl internal --module Pisth_Viewer -d https://pisth.github.io/docs/viewer/docsets/Pisth.xml -g https://github.com/ColdGrub1384/Pisth --output ../docs/viewer
-rm -r build
--- build-documentation.sh
+++
@@ -1,2 +0,0 @@
-jazzy --min-acl internal --module Pisth -d https://pisth.github.io/docs/ios/docsets/Pisth.xml -g https://github.com/ColdGrub1384/Pisth --output docs/ios
-rm -r build
