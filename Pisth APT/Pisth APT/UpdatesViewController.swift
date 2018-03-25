97cf74e759fc613143308a12c89a65b8cb163bcd
Adrian Labbe <adrilabbelol@gmail.com>
2018-03-25 11:31:30 -0300

    Removed sources list from Pisth APT

--- Pisth APT/Pisth APT/UpdatesViewController.swift
+++ Pisth APT/Pisth APT/UpdatesViewController.swift
@@ -25,7 +25,6 @@ class UpdatesViewController: UIViewController, UITableViewDataSource, UITableVie
             AppDelegate.shared.searchForUpdates()
             activityVC.dismiss(animated: true, completion: {
                 sender.endRefreshing()
-                self.tableView.reloadData()
             })
         }
         
