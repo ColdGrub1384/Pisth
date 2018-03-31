ee3d412ba7795d315fffb1711a32f330094ee6aa
Adrian Labbe <adrilabbelol@gmail.com>
2018-03-31 18:04:28 -0300

    Show licenses in Pisth APT

--- Pisth APT/Pisth APT/SettingsTableViewController.swift
+++ Pisth APT/Pisth APT/SettingsTableViewController.swift
@@ -6,8 +6,33 @@
 // See https://raw.githubusercontent.com/ColdGrub1384/Pisth/master/LICENSE for license information
 
 import UIKit
+import Pisth_Shared
 
 /// View controller to change settings.
 class SettingsTableViewController: UITableViewController {
-        
+    
+    /// Calls `dismiss(animated: true, completion: nil)`.
+    @objc func closePresented() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK: - Table view delegate
+    
+    /// Show licenses.
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.section == 1 && indexPath.row == 0 {
+            
+            // Open licenses
+            if let webVC = Bundle(for: WebViewController.self).loadNibNamed("Web", owner: nil, options: nil)?[0] as? WebViewController {
+                
+                let navVC = UINavigationController(rootViewController: webVC)
+                
+                webVC.file = Bundle.main.url(forResource: "Licenses", withExtension: "html")
+                webVC.navigationItem.leftBarButtonItems = [UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(closePresented))]
+                
+                present(navVC, animated: true, completion: nil)
+            }
+        }
+    }
+    
 }
