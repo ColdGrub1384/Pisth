a78f95cbc76d07ca0c360db7801f602f73aaa54a
Adrian Labbé <adrilabbelol@gmail.com>
2018-07-25 20:11:49 -0400

    Pisth Dark theme

---
+++ Pisth Shared/Pisth Shared/Themes/PisthDark.swift
@@ -0,0 +1,22 @@
+// This source file is part of the https://github.com/ColdGrub1384/Pisth open source project
+//
+// Copyright (c) 2017 - 2018 Adrian Labbé
+// Licensed under Apache License v2.0
+//
+// See https://raw.githubusercontent.com/ColdGrub1384/Pisth/master/LICENSE for license information
+
+import Foundation
+
+/// Dark version of `PisthTheme`.
+class PisthDarkTheme: ProTheme {
+    
+    /// Returns purple.
+    open override var foregroundColor: Color? {
+        return Color(red: 120/255, green: 32/255, blue: 157/255, alpha: 1)
+    }
+    
+    /// Returns purple.
+    open override var cursorColor: Color? {
+        return Color(red: 120/255, green: 32/255, blue: 157/255, alpha: 1)
+    }
+}
