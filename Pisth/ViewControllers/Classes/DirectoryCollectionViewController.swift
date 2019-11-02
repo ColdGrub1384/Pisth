aad55b28521c2cb4288b5866f3ed691cb726184d
Adrian Labbé <adrilabbelol@gmail.com>
2019-11-01 23:02:37 -0300

    Resize files in doc browser

--- Pisth/ViewControllers/Classes/DirectoryCollectionViewController.swift
+++ Pisth/ViewControllers/Classes/DirectoryCollectionViewController.swift
@@ -2000,7 +2000,7 @@ class DirectoryCollectionViewController: UICollectionViewController, LocalDirect
     /// Grid layout.
     static var gridLayout: UICollectionViewFlowLayout {
         let layout = UICollectionViewFlowLayout()
-        layout.itemSize = CGSize(width: 100, height: 120)
+        layout.itemSize = CGSize(width: 110, height: 130)
         layout.minimumInteritemSpacing = 1
         layout.sectionInset = UIEdgeInsets(top: 0, left: 30, bottom: 0, right: 30)
         return layout
@@ -2009,7 +2009,7 @@ class DirectoryCollectionViewController: UICollectionViewController, LocalDirect
     /// List layout.
     static func listLayout(forView view: UIView) -> UICollectionViewFlowLayout {
         let layout = UICollectionViewFlowLayout()
-        layout.itemSize = CGSize(width: view.frame.width, height: 50)
+        layout.itemSize = CGSize(width: view.frame.width, height: 40)
         return layout
     }
 }
